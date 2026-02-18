ca14dd4b9ae8aa7fbbee9971a430a846763798eb
John McWilliams <johnmcwilliams@Johns-MacBook-Air-45.local>
2026-02-18 17:37:51 -0500

    horse_personal_best

--- discussion6.py
+++ discussion6.py
@@ -118,7 +118,13 @@ class HorseRaces:
             A dictionary of tuples of each horse, with their fastest race and time.
             EXAMPLE: {"Oguri Cap": ("Tenno Sho Fall", 16.6), "Mejiro McQueen": ("Tenno Sho Fall", 16.1)}
         '''
-        pass
+
+        result_dict = {}
+        for horse in self.race_dict:
+            result_dict[horse] = self.horse_fastest_race(horse)
+        return result_dict
+
+        #pass
 
 ###############################################################################
 ##### TASK 4
